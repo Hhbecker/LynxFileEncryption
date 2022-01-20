f570c367a9b46b7948fcced4ce8ccaa1d319ce50
Hhbecker <Hbeckerhenry@gmail.com>
2022-01-19 21:05:48 -0500

    init

---
+++ driver.c
@@ -0,0 +1,24 @@
+#include "hashmap.h"    
+// link to Header file "hashmap.h" which contains:
+// linked list node struct "llnode"
+// hashmap struct "hashmap"
+
+struct hashmap* hm_create(int num_buckets){
+
+}
+
+int hm_get(struct hashmap* hm, char* word, char* document_id){
+
+}
+
+void hm_put(struct hashmap* hm, char* word, char* document_id, int num_occurrences){
+
+}
+
+void hm_destroy(struct hashmap* hm){
+
+}
+
+int hash(struct hashmap* hm, char* word, char* document_id){
+
+}
